f950001c4c9c8b7c5dde62e6fca02c15a5d75fe5
Sébastien Guimmara <sebastien.guimmara@gmail.com>
2015-05-16 00:14:35 +0200

    init: create main directories
    create the following directories:
    - objects/
    - refs/
    - branches/
    
    and the HEAD file.
    
    Signed-off-by: Sébastien Guimmara <sebastien.guimmara@gmail.com>

--- src/builtins/pit_init.py
+++ src/builtins/pit_init.py
@@ -4,6 +4,20 @@ __author__ = 'Sébastien Guimmara <sebastien.guimmara@gmail.com>'
 
 import os.path
 
-def init(worktree_dir):
-    os.mkdir(os.path.join(worktree_dir, '.git/'))
-    pass
+def init(working_tree_dir):
+
+    repo_dir = os.path.join(working_tree_dir, '.git/')
+    branches_dir = os.path.join(repo_dir, 'branches')
+    objects_dir = os.path.join(repo_dir, 'objects')
+    refs_dir = os.path.join(repo_dir, 'refs')
+
+    HEAD_file = os.path.join(repo_dir, 'HEAD')
+
+    os.mkdir(repo_dir)
+    os.mkdir(branches_dir)
+    os.mkdir(objects_dir)
+    os.mkdir(refs_dir)
+
+    os.mknod(HEAD_file, mode=666)
+
+
--- tests/builtins/test_init.py
+++ tests/builtins/test_init.py
@@ -8,7 +8,19 @@ __author__ = 'Sébastien Guimmara <sebastien.guimmara@gmail.com>'
 class TestInit(TestCase):
   def test_init(self):
       test_utils.clean_arena()
-      worktree_dir = test_utils.get_arena_dir()
-      init(worktree_dir)
-      repo_dir = os.path.join(worktree_dir, '.git/')
+      working_tree_dir = test_utils.get_arena_dir()
+      init(working_tree_dir)
+
+      repo_dir = os.path.join(working_tree_dir, '.git/')
+      branches_dir = os.path.join(repo_dir, 'branches')
+      objects_dir = os.path.join(repo_dir, 'objects')
+      refs_dir = os.path.join(repo_dir, 'refs')
+
+      HEAD_file = os.path.join(repo_dir, 'HEAD')
+
       self.assertTrue(os.path.exists(repo_dir), 'missing .git/ directory')
+      self.assertTrue(os.path.exists(branches_dir), 'missing branches/ directory')
+      self.assertTrue(os.path.exists(objects_dir), 'missing objects/ directory')
+      self.assertTrue(os.path.exists(refs_dir), 'missing refs/ directory')
+
+      self.assertTrue(os.path.exists(HEAD_file), 'missing HEAD file')
