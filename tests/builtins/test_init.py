b9277f3666202ef22b5737bcd55860807cd5eb8c
Sébastien Guimmara <sebastien.guimmara@gmail.com>
2015-05-17 13:00:00 +0200

    update_index.py: added test for pathname padding
    pathnames in index must be padded with NULs to a multiple of 8:
    
        HELLO.txt -> HELLO.txt0000000
    
    Signed-off-by: Sébastien Guimmara <sebastien.guimmara@gmail.com>

--- tests/builtins/test_init.py
+++ tests/builtins/test_init.py
@@ -5,22 +5,23 @@ from src.builtins.pit_init import init
 
 __author__ = 'Sébastien Guimmara <sebastien.guimmara@gmail.com>'
 
+
 class TestInit(TestCase):
-  def test_init(self):
-      test_utils.clean_arena()
-      working_tree_dir = test_utils.get_arena_dir()
-      init(working_tree_dir)
+    def test_init(self):
+        test_utils.clean_arena()
+        working_tree_dir = test_utils.get_arena_dir()
+        init(working_tree_dir)
 
-      repo_dir = os.path.join(working_tree_dir, '.git/')
-      branches_dir = os.path.join(repo_dir, 'branches')
-      objects_dir = os.path.join(repo_dir, 'objects')
-      refs_dir = os.path.join(repo_dir, 'refs')
+        repo_dir = os.path.join(working_tree_dir, '.git/')
+        branches_dir = os.path.join(repo_dir, 'branches')
+        objects_dir = os.path.join(repo_dir, 'objects')
+        refs_dir = os.path.join(repo_dir, 'refs')
 
-      HEAD_file = os.path.join(repo_dir, 'HEAD')
+        HEAD_file = os.path.join(repo_dir, 'HEAD')
 
-      self.assertTrue(os.path.exists(repo_dir), 'missing .git/ directory')
-      self.assertTrue(os.path.exists(branches_dir), 'missing branches/ directory')
-      self.assertTrue(os.path.exists(objects_dir), 'missing objects/ directory')
-      self.assertTrue(os.path.exists(refs_dir), 'missing refs/ directory')
+        self.assertTrue(os.path.exists(repo_dir), 'missing .git/ directory')
+        self.assertTrue(os.path.exists(branches_dir), 'missing branches/ directory')
+        self.assertTrue(os.path.exists(objects_dir), 'missing objects/ directory')
+        self.assertTrue(os.path.exists(refs_dir), 'missing refs/ directory')
 
-      self.assertTrue(os.path.exists(HEAD_file), 'missing HEAD file')
+        self.assertTrue(os.path.exists(HEAD_file), 'missing HEAD file')
--- tests/builtins/test_update_index.py
+++ tests/builtins/test_update_index.py
@@ -6,18 +6,13 @@ __author__ = 'Sébastien Guimmara <sebastien.guimmara@gmail.com>'
 
 
 class TestUpdateIndex(TestCase):
-  def test_index_header_is_dirc(self):
-    ctx = test_utils.setup_repo()
+    def test_index_header_is_dirc(self):
+        ctx = test_utils.setup_repo()
+        hellofile = test_utils.create_arena_file('hello\n', 'HELLO.txt')
+        index.update_index(ctx, [hellofile])
+        index_content = open(ctx.index, 'rb').read()
 
-    index.update_index(ctx, [])
-
-    index_content = open(ctx.index, 'rb').read()
-
-    self.assertEqual('DIRC'.encode(), index_content[:4])
-
-  def test_correct_sha1_blob(self):
-    hellofile = test_utils.create_arena_file('hello\n', 'HELLO.txt')
-    ctx = test_utils.get_arena_context()
-    index.update_index(ctx, [hellofile])
-    index_content = open(ctx.index, 'rb').read()
-    self.assertEqual(b'ce013625030ba8dba906f756967f9e9ca394464a', index_content[52:][:40])
+        self.assertEqual('DIRC'.encode(), index_content[:4], 'incorrect index header (must be DIRC)')
+        self.assertEqual(b'ce013625030ba8dba906f756967f9e9ca394464a', index_content[52:][:40], 'incorrect SHA-1')
+        self.assertEqual('HELLO.txt\x00\x00\x00\x00\x00\x00\x00', index_content[94:][:16].decode(), 'incorrect padding'
+                                                                                                    ' of filename')
