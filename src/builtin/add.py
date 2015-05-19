63ecff08c955917f766b84f05939e2c4772e57ac
Sébastien Guimmara <sebastien.guimmara@gmail.com>
2015-05-19 03:34:46 +0200

    add: implement add()
    add() adds the passed pathnames to the index. If a pathname is invalid,
    dies with an error message.
    
    Signed-off-by: Sébastien Guimmara <sebastien.guimmara@gmail.com>

---
+++ src/builtin/add.py
@@ -0,0 +1,30 @@
+# coding: utf-8
+
+"""
+Pit - Git in python
+
+Copyright (C) 2015  Sébastien Guimmara
+
+add.py: add files to index
+"""
+
+__author__ = 'Sébastien Guimmara <sebastien.guimmara@gmail.com>'
+
+import context
+import os
+from builtin import index
+import sys
+
+def add(argv):
+    """
+    'pit add' expects a list of files to add and optional arguments
+    """
+    ctx = context.get_context()
+    files_to_add = argv
+    entries = index.get_entries(pathnames_only=True)
+    for f in files_to_add:
+        if not os.path.exists(os.path.join(ctx.working_dir, f)):
+            print('fatal: pathspec "%s" did not match any files' % f)
+            sys.exit(2)
+
+    index.update_index(entries + files_to_add)
---
+++ tests/builtin/test_add.py
@@ -0,0 +1,19 @@
+from unittest import TestCase
+import tests.test_utils
+from src.builtin import index, add
+
+__author__ = 'Sébastien Guimmara <sebastien.guimmara@gmail.com>'
+
+
+class TestAdd(TestCase):
+    def test_add(self):
+        tests.test_utils.setup_repo()
+        file_a = tests.test_utils.create_arena_file('', 'AAAA')
+        file_b = tests.test_utils.create_arena_file('', 'BBBB')
+        file_c = tests.test_utils.create_arena_file('', 'CCCC')
+        index.update_index([file_a, file_b, file_c])
+        file_d = tests.test_utils.create_arena_file('', 'DDDD')
+        add.add([file_d])
+        actual_entries = index.get_entries(pathnames_only=True)
+        expected_entries = ['AAAA', 'BBBB', 'CCCC', 'DDDD']
+        self.assertEqual(actual_entries, expected_entries)
